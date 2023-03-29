8a071cddca3c20bf8d871d87dfe8df45b5310603
AfoD3V <romanafonin@protonmail.com>
2023-03-29 20:41:54 +0200

    Final Genetic Algorithm

--- fitness.py
+++ fitness.py
@@ -5,11 +5,12 @@ import population
 def calculate_fitness(backpack: list, backpack_indx: int):
     """
     :param backpack: array of 1 and 0.
+    :backpack_index: index for a backpack on a list of backpack's
     :return: Total weight and price of items in backpack
     """
     weight = 0
     price = 0
-    for count, item in enumerate(list(backpack[backpack_indx])):
+    for count, item in enumerate(list(backpack)):
         if int(item) == 1:
             weight += utils.INPUT_DATA_DICT.get(count).weight
             price += utils.INPUT_DATA_DICT.get(count).price
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from population import generate_population
 
 # Define the parameters
 population_size = 6
-num_generations = 2
+num_generations = 100
 num_parents_mating = 2
 sol_per_pop = 20
 num_genes = len(utils.INPUT_DATA_DICT)
@@ -14,45 +14,51 @@ mutation_probability = 0.1
 
 # Create an instance of the GA class
 ga_instance = pygad.GA(
+    initial_population=generate_population(population_size),
     num_generations=num_generations,
     num_parents_mating=num_parents_mating,
-    sol_per_pop=sol_per_pop,
     num_genes=num_genes,
+    parent_selection_type="tournament",
+    crossover_type="single_point",
+    mutation_type="swap",
     crossover_probability=crossover_probability,
     mutation_probability=mutation_probability,
     fitness_func=calculate_fitness,
 )
 
-# Run the GA
-ga_instance.run()
+if __name__ == "__main__":
+    # Run the GA
+    ga_instance.run()
+    solution, solution_fitness, solution_idx = ga_instance.best_solution()
 
-# Print the best solution
-solution, solution_fitness, solution_idx = ga_instance.best_solution()
-binary_solution = [1 if x >= 0.5 else 0 for x in solution]
-ga_instance.plot_fitness()
-print("Best solution (binary):", binary_solution)
+    # Binary solution otherwise as a result we are going to get float vector values
+    binary_solution = [1 if x >= 0.5 else 0 for x in solution]
 
-print(
-    "Total value: ",
-    sum(
+    # Print the best solution
+    print(f"Best binary solution: {binary_solution}")
+    print("Fitness score:", solution_fitness)
+
+    # list comprehension to sum value of all items
+    total_items_value = sum(
         [
             utils.INPUT_DATA_DICT.get(i).price
             for i in range(len(binary_solution))
             if binary_solution[i] == 1
             and utils.INPUT_DATA_DICT.get(i).price is not None
         ]
-    ),
-)
-print(
-    "Total weight: ",
-    sum(
+    )
+    print(f"Total value of items: {total_items_value}")
+
+    # list comprehension to sum total weight of all items
+    total_weight_value = sum(
         [
             utils.INPUT_DATA_DICT.get(i).weight
             for i in range(len(binary_solution))
             if binary_solution[i] == 1
             and utils.INPUT_DATA_DICT.get(i).weight is not None
         ]
-    ),
-)
+    )
+    print(f"Total weigth of items: {total_weight_value}")
 
-print("Best solution fitness:", solution_fitness)
+    # Generate plot for visualization
+    ga_instance.plot_fitness()
