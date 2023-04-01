1e35753245b8cd904a27b6823fbf3a956c25f5bf
mbabi <mbabiak39@gmail.com>
2023-04-01 11:34:06 +0200

    changing main loop

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from fitness import calculate_fitness
 from population import generate_population
 
 # Define the parameters
-population_size = 6
-num_generations = 100
+population_size = 10
+num_generations = 1
 num_parents_mating = 2
 sol_per_pop = 20
 num_genes = utils.CHROMOSOME_COUNT
@@ -18,47 +18,54 @@ ga_instance = pygad.GA(
     num_generations=num_generations,
     num_parents_mating=num_parents_mating,
     num_genes=num_genes,
-    parent_selection_type="tournament",
-    crossover_type="single_point",
-    mutation_type="swap",
+    parent_selection_type="rws",
+    crossover_type="two_points",
+    mutation_type="scramble",
     crossover_probability=crossover_probability,
     mutation_probability=mutation_probability,
     fitness_func=calculate_fitness,
 )
 
 if __name__ == "__main__":
-    # Run the GA
-    ga_instance.run()
-    solution, solution_fitness, solution_idx = ga_instance.best_solution()
+    GENERATIONS = 0
+    while GENERATIONS <= 100:
+        # Run the GA
+        print(f'Generation num {GENERATIONS}, Initial Pop: {ga_instance.initial_population}')
+        ga_instance.run()
+        solution, solution_fitness, solution_idx = ga_instance.best_solution()
+        print(f'Result pop {ga_instance.population}')
 
-    # Binary solution otherwise as a result we are going to get float vector values
-    binary_solution = [1 if x >= 0.5 else 0 for x in solution]
+        # Binary solution otherwise as a result we are going to get float vector values
+        binary_solution = [1 if x >= 0.5 else 0 for x in solution]
 
-    # Print the best solution
-    print(f"Best binary solution: {binary_solution}")
-    print("Fitness score:", solution_fitness)
+        # Print the best solution
+        print(f"Best binary solution: {binary_solution}")
+        print("Fitness score:", solution_fitness)
 
-    # list comprehension to sum value of all items
-    total_items_value = sum(
-        [
-            utils.INPUT_DATA_DICT.get(i).price
-            for i in range(len(binary_solution))
-            if binary_solution[i] == 1
-            and utils.INPUT_DATA_DICT.get(i).price is not None
-        ]
-    )
-    print(f"Total value of items: {total_items_value}")
+        # list comprehension to sum value of all items
+        total_items_value = sum(
+            [
+                utils.INPUT_DATA_DICT.get(i).price
+                for i in range(len(binary_solution))
+                if binary_solution[i] == 1
+                   and utils.INPUT_DATA_DICT.get(i).price is not None
+            ]
+        )
+        print(f"Total value of items: {total_items_value}")
 
-    # list comprehension to sum total weight of all items
-    total_weight_value = sum(
-        [
-            utils.INPUT_DATA_DICT.get(i).weight
-            for i in range(len(binary_solution))
-            if binary_solution[i] == 1
-            and utils.INPUT_DATA_DICT.get(i).weight is not None
-        ]
-    )
-    print(f"Total weigth of items: {total_weight_value}")
+        # list comprehension to sum total weight of all items
+        total_weight_value = sum(
+            [
+                utils.INPUT_DATA_DICT.get(i).weight
+                for i in range(len(binary_solution))
+                if binary_solution[i] == 1
+                   and utils.INPUT_DATA_DICT.get(i).weight is not None
+            ]
+        )
+        print(f"Total weigth of items: {total_weight_value}")
 
-    # Generate plot for visualization
-    ga_instance.plot_fitness()
+        # Generate plot for visualization
+        ga_instance.plot_fitness(save_dir=f"results/Generation{GENERATIONS}")
+        # Prepare for nextgen
+        ga_instance.initial_population = ga_instance.population
+        GENERATIONS += 1
